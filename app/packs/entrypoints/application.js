4a6942b5934f595544f8ffcd13b5893f09e9c19c
Eric-Guo <eric.guocz@gmail.com>
2021-08-23 11:23:18 +0800

    Add tailwindcss 2.2.7 JIT.
    Fix tailwind

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -7,5 +7,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "stylesheets/application.scss"
+
 Rails.start()
 ActiveStorage.start()
