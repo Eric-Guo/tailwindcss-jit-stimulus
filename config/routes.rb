d911b70c0fa422e0f57c3fcc703157bbf9ebe22a
Eric-Guo <eric.guocz@gmail.com>
2021-08-23 11:27:39 +0800

    Add home page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  root "home#index"
 end
