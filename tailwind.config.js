a23d422c0e0df4d7b746bce7617ccc47384ce77c
Eric-Guo <eric.guocz@gmail.com>
2021-08-26 10:09:16 +0800

    天华材料库前端

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat', 'Source Han Sans', 'Source Han Sans SC', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', 'sans-serif'],
       },
+      screens: {
+        '3xl': '1850px',
+      },
+      gridTemplateColumns: {
+        // Simple 20 column grid
+        '20': 'repeat(20, minmax(0, 1fr))',
+      },
+      visibility: ['hover','focus'],
     },
   },
   plugins: [
