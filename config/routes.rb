43bfe4c18eb9ba98007da2c3f76c12b412d90b3f
Eric-Guo <eric.guocz@gmail.com>
2021-08-26 10:09:16 +0800

    天华材料库前端

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  root "home#index"
+Rails.application.routes.draw do
+  resources :samples, only: [:show]
+
+  root 'home#index'
 end
