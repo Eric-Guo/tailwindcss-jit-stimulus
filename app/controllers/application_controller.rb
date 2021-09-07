3a8e6272ed94bdaa43f78058fdd5d832c09cff60
Eric-Guo <eric.guocz@gmail.com>
2021-09-07 14:29:06 +0800

    Add IE warning.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_ie_warning
+
+  private
+
+    def set_ie_warning
+      if browser.ie?
+        flash.now[:alert] = "本站点必须在Chrome, Edge, Firefox等非IE浏览器下浏览，Chrome浏览器可以从<a href='https://www.google.cn/intl/zh-CN/chrome/'>https://www.google.cn/intl/zh-CN/chrome/</a>下载。".html_safe
+      end
+    end
 end
