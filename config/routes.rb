c2ca6a95ce3d530c3368e68f829951ae773863d5
Eric-Guo <eric.guocz@gmail.com>
2021-09-14 14:06:26 +0800

    Add four result page of search.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :samples, only: [:show]
   resources :materials, only: [:show]
 
-  resource :search, only: [:show]
+  resource :search, only: [:show] do
+    get :material
+    get :project
+    get :manufacturer
+    get :news
+  end
   root 'home#index'
 end
