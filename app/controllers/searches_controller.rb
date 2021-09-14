3975534528437a290f67d78c1b6a846db354f5f8
Eric-Guo <eric.guocz@gmail.com>
2021-09-14 14:06:26 +0800

    Add four result page of search.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,18 @@
 
 class SearchesController < ApplicationController
   def show
+    redirect_to material_search_path
+  end
+
+  def material
+  end
+
+  def project
+  end
+
+  def manufacturer
+  end
+
+  def news
   end
 end
