94a192adf54578f7812b13a543d52f54bcaed5ce
Eric-Guo <eric.guocz@gmail.com>
2021-09-16 17:22:24 +0800

    Text change.

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   resources :samples, only: [:show]
   resources :materials, only: [:show]
 
-  resource :search, only: [:show] do
+  resource :search, only: [] do
     get :material
     get :project
     get :manufacturer
     get :news
   end
+
   root 'home#index'
 end
