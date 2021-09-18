20a029d69274e80f6ffdfcd2ebf898156817275c
Eric-Guo <eric.guocz@gmail.com>
2021-09-18 13:49:52 +0800

    Remove hello and add search type select.

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -4,6 +4,6 @@ class HomesTest < ApplicationSystemTestCase
   test "visiting the root" do
     visit root_url
     wait_for_stimulus_loading
-    assert_selector "h2", text: "Welcome to"
+    assert_selector "h2", text: "Welcome!"
   end
 end
