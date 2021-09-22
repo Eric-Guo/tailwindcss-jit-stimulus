09ce5a1f3c7ec808dfac1c96b35370f062f79056
Eric-Guo <eric.guocz@gmail.com>
2021-09-22 13:42:58 +0800

    Add slideover

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -22,4 +22,9 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 window.Stimulus = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
+stimulus.load(definitionsFromContext(context))
+
+import { Slideover } from "tailwindcss-stimulus-components"
+stimulus.register('slideover', Slideover)
+
+window.Stimulus = stimulus;
