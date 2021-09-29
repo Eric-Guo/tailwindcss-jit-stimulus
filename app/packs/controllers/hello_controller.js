c8fce0370d736d36916594225cdf6a2fac0e6364
Eric-Guo <eric.guocz@gmail.com>
2021-09-29 10:45:23 +0800

    Upgrade to tailwindcss-stimulus-components v2.3.0

--- app/packs/controllers/hello_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = [ "output" ]
-
-  connect() {
-    this.outputTarget.textContent = 'Welcome to'
-  }
-}
