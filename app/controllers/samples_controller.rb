50395c11c68fe696a34de223dcadc025da46719a
Eric-Guo <eric.guocz@gmail.com>
2021-10-11 14:53:43 +0800

    Stile improvement.
    (cherry picked from commit 7fe5273d19f88c4bfebae373d3f7449db3560a58)

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -5,10 +5,9 @@ class SamplesController < ApplicationController
     @sample = Sample.find_by!(no: params[:id])
 
     # 所属案例
-    @caseIds = CasesMaterial.where(type:1, sample_id:@sample.id).pluck(:case_id)
-    @cases = Cases.where(id: @caseIds)
+    case_ids = CasesMaterial.where(type: 1, sample_id: @sample.id).pluck(:case_id)
+    @cases = Cases.where(id: case_ids)
 
     # 其他样品
-
   end
 end
