ad52da63de9f1349f7a82ceb985b49d2c9b4cc19
秦嘉鹏 <759848795@qq.com>
2021-10-14 12:26:23 +0800

    菜单导航完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include DetectDevice
   before_action :set_ie_warning
+  before_action :set_sidebar_nav
 
   private
 
@@ -8,5 +9,63 @@ class ApplicationController < ActionController::Base
       if @browser.ie?
         flash.now[:alert] = "本站点必须在Chrome, Edge, Firefox等非IE浏览器下浏览，Chrome浏览器可以从<a href='https://www.google.cn/intl/zh-CN/chrome/'>https://www.google.cn/intl/zh-CN/chrome/</a>下载。".html_safe
       end
+    end
+
+    def set_sidebar_nav
+      @sidebar_nav = [
+        {
+          title: '材料',
+          subtitle: 'Materials',
+          children: [
+            {
+              title: '石材',
+              subtitle: 'Stone',
+              children: [
+                {
+                  title: '石灰石/青石',
+                  subtitle: 'Limestone',
+                  url: '',
+                },
+                {
+                  title: '大理石',
+                  subtitle: 'Marble',
+                  url: '',
+                }
+              ]
+            },
+            {
+              title: '玻璃',
+              subtitle: 'Glass',
+              children: [
+                {
+                  title: '超白透明色玻璃基片',
+                  subtitle: '',
+                  url: '',
+                },
+                {
+                  title: '磨砂双星玻璃砖',
+                  subtitle: '',
+                  url: '',
+                },
+              ]
+            }
+          ]
+        },
+        {
+          title: '案例',
+          subtitle: 'Projects',
+          url: '',
+        },
+        {
+          title: '供应商',
+          subtitle: 'Manufacturer',
+          url: '',
+        },
+        {
+          title: '需求登记',
+          subtitle: 'Your Demand',
+          url: '',
+        }
+      ]
     end
 end
