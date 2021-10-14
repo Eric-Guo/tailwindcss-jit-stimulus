a923c63dcac5197ae2786e860aa9cf464fc855be
Eric-Guo <eric.guocz@gmail.com>
2021-10-14 14:49:42 +0800

    Fix model declare.
    (cherry picked from commit e4b24cbba8687ea28a0f815eaa0d1e741f6273fc)

--- app/models/material_product_color_system.rb
+++ app/models/material_product_color_system.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class MaterialProductColorSystem < ApplicationRecord
-  belongs_to :material_product,foreign_key: :color_systems_id
-  belongs_to :color_system,foreign_key: :color_systems_id
+  belongs_to :material_product
+  belongs_to :color_system, foreign_key: :color_systems_id
 end
