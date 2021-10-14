bebaff3761932d27f0f6af068c87ce4de7359aee
Eric-Guo <eric.guocz@gmail.com>
2021-10-14 15:38:42 +0800

    Show color systems.

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,6 +3,7 @@
 class Material < ApplicationRecord
   has_one :material_info
   has_one :material_product, foreign_key: :material_id, class_name: 'MaterialProduct'
+  has_many :children_materials, class_name: :Material, foreign_key: :parent_id
 
   def color
     if level == 3
@@ -10,5 +11,20 @@ class Material < ApplicationRecord
     elsif level == 2
       material_info.color_system.description
     end
+  end
+
+  def parent_material
+    case level
+    when 3
+      Material.find_by(id: parent_id)
+    when 2
+      self
+    end
+  end
+
+  def parent_color_systems
+    parent_material.children_materials.collect do |m|
+      m.material_product.color_systems
+    end.flatten.uniq
   end
 end
