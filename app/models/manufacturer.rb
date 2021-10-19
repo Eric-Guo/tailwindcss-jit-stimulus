73136577800f9866a22c453fef9433108024a796
ringo <ringobool@outlook.com>
2021-10-19 10:37:14 +0800

    产品厂家样品附件下载
    (cherry picked from commit b0831f1f4dfdc7463905ada1e325427e3872dcda)

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,7 @@
 
 class Manufacturer < ApplicationRecord
   has_many :samples
+
+  has_many :material_manufacturers, primary_key: :manufacturer_id
+  has_many :manufacturers, through: :material_manufacturers
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -6,7 +6,7 @@ class Sample < ApplicationRecord
   belongs_to :material, foreign_key: :obj_id, class_name: 'Material'
   belongs_to :surface_effect
   belongs_to :manufacturer
-  has_many :SampleEnclosures, foreign_key: :id, class_name: 'SampleEnclosure'
+  has_many :sample_enclosures
 
   has_many :sample_color_systems
   has_many :color_systems, through: :sample_color_systems
