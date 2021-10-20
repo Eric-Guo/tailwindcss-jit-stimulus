9f3dcb061a596d48140e34e935f383d7a5b17df3
秦嘉鹏 <759848795@qq.com>
2021-10-20 13:56:23 +0800

    材料详情页使用turbo重构

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -5,21 +5,36 @@ class MaterialsController < ApplicationController
     @material = Material.find_by!(id: params[:id])
 
     @color_id = params["color_id"]&.strip
+    @projects = get_color_system_projects(@material, @color_id)
 
-    @projects = Material.joins(:material_product)
-
-    if @color_id.present?
-      @projects = @projects.where(material_product: { id: MaterialProductColorSystem.select(:material_product_id).where(color_systems_id: @color_id) })
+    if @material.level == 3
+      @samples = Sample.where(obj_id: @material.id).all
+      @cases = Cases.limit(2)
     end
-  
-    case @material.level
-    when 2
-      @projects = @projects.where(parent_id: @material.id)
-    when 3
-      @projects = @projects.where(parent_id: @material.parent_id)
-    end
-
-    @samples = Sample.where(obj_id: @material.id).all
-    @cases = Cases.limit(2)
   end
+
+  def color_system_list
+    @material = Material.find(params[:id])
+    @color_id = params["color_id"]&.strip
+    @projects = get_color_system_projects(@material, @color_id)
+  end
+
+  private
+    def get_color_system_projects(material, color_id = nil)
+      projects = Material.joins(:material_product)
+
+      if color_id.present?
+        projects = projects.where(material_product: { id: MaterialProductColorSystem.select(:material_product_id).where(color_systems_id: color_id) })
+      end
+    
+      case material.level
+      when 1
+        projects = projects.where(parent_id: Material.select(:id).where(parent_id: material.id))
+      when 2
+        projects = projects.where(parent_id: material.id)
+      when 3
+        projects = projects.where(parent_id: material.parent_id)
+      end
+      projects
+    end
 end
