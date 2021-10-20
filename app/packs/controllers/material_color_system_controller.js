c1c9e17ac3113ad9b323db36748a0664a3304a93
秦嘉鹏 <759848795@qq.com>
2021-10-20 14:52:54 +0800

    增加材料详情色系超出后滚动
    (cherry picked from commit dd1bf5fe957861c92ae0ad68a7140223a4738b11)

--- app/packs/controllers/material_color_system_controller.js
+++ app/packs/controllers/material_color_system_controller.js
@@ -1,10 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['list', 'toggleBtn']
+  static targets = ['list', 'toggleBtn', 'navItem', 'nav']
   static values = {
     minHeight: Number,
     maxHeight: Number,
+  }
+
+  connect() {
+    const activeNavItem = this.getActiveNavItem();
+    if (activeNavItem) {
+      activeNavItem.scrollIntoView(false)
+    }
+  }
+  
+  getActiveNavItem() {
+    return this.navItemTargets.find(item => item.classList.contains('active-pill'));
   }
 
   toggle() {
@@ -15,6 +26,18 @@ export default class extends Controller {
     } else {
       this.listTarget.style.height = `${this.minHeightValue}px`;
       if (this.toggleBtnTarget.classList.contains('rotate-180')) this.toggleBtnTarget.classList.remove('rotate-180');
+    }
+  }
+
+  navScroll(e) {
+    if (!e.altKey) {
+      const scrollLeft = this.navTarget.scrollLeft;
+      const scrollWidth = this.navTarget.scrollWidth;
+      const width = this.navTarget.offsetWidth;
+      if ((e.deltaY > 0 && width + scrollLeft < scrollWidth) || (e.deltaY < 0 && scrollLeft > 0)) {
+        e.preventDefault();
+        this.navTarget.scrollTo({ left: scrollLeft + e.deltaY, behavior: 'smooth' });
+      }
     }
   }
 }
