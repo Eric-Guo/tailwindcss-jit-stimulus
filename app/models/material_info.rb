f6f3bf2151f3557578b6ec5fca0f2c5042fca27c
秦嘉鹏 <759848795@qq.com>
2021-10-21 17:25:41 +0800

    fixed

--- app/models/material_info.rb
+++ app/models/material_info.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MaterialInfo < ApplicationRecord
-  belongs_to :material, 
+  belongs_to :material
   belongs_to :color_system
 
   default_scope { where(deleted_at: nil) }
