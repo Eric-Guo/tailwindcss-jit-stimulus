f683d1dc42fe2562e2e2363156f7185896f407d9
秦嘉鹏 <759848795@qq.com>
2021-10-21 13:39:43 +0800

    优化材料页面部分数据显示
    (cherry picked from commit e7407b7525852db063d820dbb12ffa301d4d7fd2)

--- app/models/material_product.rb
+++ app/models/material_product.rb
@@ -14,4 +14,28 @@ class MaterialProduct < ApplicationRecord
   has_many :practical_applications, through: :material_product_practical_applications
 
   default_scope { where(deleted_at: nil) }
+
+  def construction
+    if practice_details.present?
+      JSON.parse(practice_details)
+    else
+      []
+    end
+  end
+
+  def files
+    if source_file.present?
+      JSON.parse(source_file)
+    else
+      []
+    end
+  end
+  
+  def texture
+    if su_picture.present?
+      JSON.parse(su_picture)
+    else
+      []
+    end
+  end
 end
