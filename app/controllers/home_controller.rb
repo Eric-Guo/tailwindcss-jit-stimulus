4c1cfdda44d1c5975846695a126a2f61fa1d6260
秦嘉鹏 <759848795@qq.com>
2021-11-02 16:44:03 +0800

    对接首页部分数据
    (cherry picked from commit bb4e41854336f7efa2be00ec18759554fea8f715)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@
 
 class HomeController < ApplicationController
   def index
+    @total = {
+      material: Material.count,
+      project: Cases.count,
+      manufacturer: Manufacturer.count,
+    }
+
+    @recently_materials = Material.where('level = ?', 3).order(created_at: :desc).limit(16)
+    
     @latest_news = News.order(updated_at: :desc).limit(4)
   end
 end
