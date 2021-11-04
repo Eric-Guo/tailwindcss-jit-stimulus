b62ad0f6afe49e15b98c2882af6d6801c5b935d9
秦嘉鹏 <759848795@qq.com>
2021-11-04 18:19:15 +0800

    优化部分组件样式
    (cherry picked from commit bc48a801c878ef60e73c9a683409ee093b6a94af)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,22 @@ class HomeController < ApplicationController
       manufacturer: Manufacturer.count,
     }
 
-    @recently_materials = Material.where('level = ?', 3).order(created_at: :desc).limit(16)
+    recently_material = Material.where('level = ?', 3).order(created_at: :desc).first
+    @recently_materials = if recently_material.present?
+      Material.where('level = ?', 3).order(created_at: :desc).where("date_format(created_at, '%Y%m%d') = ?", recently_material.created_at.strftime('%Y%m%d'))
+    else
+      Material.none
+    end
 
-    @recently_projects = Cases.order(created_at: :desc).limit(10)
-    
+    recently_project = Cases.order(created_at: :desc).first
+    @recently_projects = if recently_project.present?
+      Cases.order(created_at: :desc).where("", recently_project.created_at.strftime("%Y%m%d"))
+    else
+      Cases.none
+    end
+
     @latest_news = News.order(updated_at: :desc).limit(4)
+
+    @projects = Cases.order(created_at: :desc).limit(8)
   end
 end
