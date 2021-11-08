fff6ccc9bb72be1456ee2add9c5436ad2fc9167e
秦嘉鹏 <759848795@qq.com>
2021-11-08 16:05:13 +0800

    修改首页材料数据

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,6 +30,19 @@ class HomeController < ApplicationController
 
     @projects = Cases.order(top_at: :desc).limit(8)
 
+    @material_cates = [
+      { title: '石材', subtitle: 'Stone', cover: 'mat_nav_g1.jpg' },
+      { title: '玻璃', subtitle: 'Glass', cover: 'mat_nav_g2.jpg' },
+      { title: '陶瓷', subtitle: 'Ceramic', cover: 'mat_nav_g3.jpg' },
+      { title: '混凝土/水泥', subtitle: 'Concrete/Cement', cover: 'mat_nav_g4.jpg' },
+      { title: '木材', subtitle: 'Wood', cover: 'mat_nav_g5.jpg' },
+      { title: '金属', subtitle: 'Mental', cover: 'mat_nav_g6.jpg' },
+      { title: '涂料', subtitle: 'Paint', cover: 'mat_nav_g7.jpg' },
+      { title: '高分子材料', subtitle: 'Plastic', cover: 'mat_nav_g8.jpg' },
+      { title: '生态', subtitle: 'Ecology', cover: 'mat_nav_g9.jpg' },
+      { title: '新材料', subtitle: 'New Materials', cover: 'mat_nav_g10.jpg' },
+    ]
+
     @manufacturer_cates = Material.where(level: 1)
   end
 end
