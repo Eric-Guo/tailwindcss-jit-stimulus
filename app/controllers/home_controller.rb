1f3b6d69c0a438e53de0ae24f18b666ed4593f86
Eric-Guo <eric.guocz@gmail.com>
2021-11-08 16:44:49 +0800

    Link to materials.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,16 +31,16 @@ class HomeController < ApplicationController
     @projects = Cases.order(top_at: :desc).limit(8)
 
     @material_cates = [
-      { title: '石材', subtitle: 'Stone', cover: 'mat_nav_g1.jpg' },
-      { title: '玻璃', subtitle: 'Glass', cover: 'mat_nav_g2.jpg' },
-      { title: '陶瓷', subtitle: 'Ceramic', cover: 'mat_nav_g3.jpg' },
-      { title: '混凝土/水泥', subtitle: 'Concrete/Cement', cover: 'mat_nav_g4.jpg' },
-      { title: '木材', subtitle: 'Wood', cover: 'mat_nav_g5.jpg' },
-      { title: '金属', subtitle: 'Mental', cover: 'mat_nav_g6.jpg' },
-      { title: '涂料', subtitle: 'Paint', cover: 'mat_nav_g7.jpg' },
-      { title: '高分子材料', subtitle: 'Plastic', cover: 'mat_nav_g8.jpg' },
-      { title: '生态', subtitle: 'Ecology', cover: 'mat_nav_g9.jpg' },
-      { title: '新材料', subtitle: 'New Materials', cover: 'mat_nav_g10.jpg' },
+      { mat: Material.find_by(name: '石材'), title: '石材', subtitle: 'Stone', cover: 'mat_nav_g1.jpg' },
+      { mat: Material.find_by(name: '玻璃'), title: '玻璃', subtitle: 'Glass', cover: 'mat_nav_g2.jpg' },
+      { mat: Material.find_by(name: '陶瓷'), title: '陶瓷', subtitle: 'Ceramic', cover: 'mat_nav_g3.jpg' },
+      { mat: Material.find_by(name: '水泥'), title: '混凝土/水泥', subtitle: 'Concrete/Cement', cover: 'mat_nav_g4.jpg' },
+      { mat: Material.find_by(name: '木材'), title: '木材', subtitle: 'Wood', cover: 'mat_nav_g5.jpg' },
+      { mat: Material.find_by(name: '金属'), title: '金属', subtitle: 'Mental', cover: 'mat_nav_g6.jpg' },
+      { mat: Material.find_by(name: '涂料'), title: '涂料', subtitle: 'Paint', cover: 'mat_nav_g7.jpg' },
+      { mat: Material.find_by(name: '高分子材料'), title: '高分子材料', subtitle: 'Plastic', cover: 'mat_nav_g8.jpg' },
+      { mat: Material.find_by(name: '生态'), title: '生态', subtitle: 'Ecology', cover: 'mat_nav_g9.jpg' },
+      { mat: Material.find_by(name: '新型'), title: '新材料', subtitle: 'New Materials', cover: 'mat_nav_g10.jpg' },
     ]
 
     @manufacturer_cates = Material.where(level: 1)
