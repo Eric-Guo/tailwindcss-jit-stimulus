48d4153601098c6e8ff3c63243d7c225b6e798cd
Eric-Guo <eric.guocz@gmail.com>
2021-11-16 15:51:09 +0800

    Extend the cases search.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -31,7 +31,8 @@ class SearchesController < ApplicationController
       end
 
       @cases = if @q.present?
-        Cases.where('project_name LIKE ?', "%#{@q}%")
+        Cases.where('project_name LIKE ? OR business_type LIKE ? OR project_type LIKE ? OR project_location LIKE ? OR design_unit LIKE ?',
+         "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%")
       else
         Cases.none
       end
