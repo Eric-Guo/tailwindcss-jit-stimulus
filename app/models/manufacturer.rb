15f61287e9d999880f95e934cc2665964c06a169
秦嘉鹏 <759848795@qq.com>
2021-11-17 19:04:04 +0800

    修改供应商搜索显示顺序为: 有logo愿意>有logo不愿意>无logo愿意>无logo不愿意
    (cherry picked from commit 95f21777a73c33af7b0003f7b455ba33738abbd3)

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -7,4 +7,8 @@ class Manufacturer < ApplicationRecord
   has_many :materials, through: :material_manufacturers
 
   default_scope { where(deleted_at: nil) }
+
+  def self.sort_by_logo(sort = :desc)
+    order(Arel.sql("CASE WHEN logo IS NULL THEN 0 WHEN logo = '' THEN 1 ELSE 2 END #{sort}"))
+  end
 end
