d387cd40140cfead5359744e9e8d1c2c05f3ee09
Eric-Guo <eric.guocz@gmail.com>
2021-11-19 16:00:26 +0800

    Copy case view from search.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,12 @@
 
 class ProjectsController < ApplicationController
   def index
+    @q = '上海'
+    @cases = if @q.present?
+      Cases.where('project_name LIKE ? OR business_type LIKE ? OR project_type LIKE ? OR project_location LIKE ? OR design_unit LIKE ?',
+        "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%")
+    else
+      Cases.none
+    end
   end
 end
