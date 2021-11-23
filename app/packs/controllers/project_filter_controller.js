67941af19ceffef68500aa809345532389d0007a
Eric-Guo <eric.guocz@gmail.com>
2021-11-23 13:58:02 +0800

    Add select all for UI.
    (cherry picked from commit 139f345fad7b862f6b4e459f8131f02c484593f1)

--- app/packs/controllers/project_filter_controller.js
+++ app/packs/controllers/project_filter_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['selectAll']
+  static targets = ['selectAll','selectItem']
 
   connect() {
     this.selectAllTarget.indeterminate  = true;
@@ -12,6 +12,15 @@ export default class extends Controller {
 
   remove(e) {
     e.target.parentNode.remove();
+    e.preventDefault();
+  }
+
+  select_all(e) {
+    if(this.selectAllTarget.checked) {
+      this.selectItemTargets.map(function(t) { t.checked = true; });
+    } else {
+      this.selectItemTargets.map(function(t) { t.checked = false; });
+    }
     e.preventDefault();
   }
 }
