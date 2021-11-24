ce4994da207fff8cf51a24039fc09133c77d436c
Eric-Guo <eric.guocz@gmail.com>
2021-11-24 17:31:08 +0800

    Enable change based on material type.
    (cherry picked from commit a6cf0390ee4c9a8ccb9973489ed2b85f8cac3718)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,7 @@
 class ProjectsController < ApplicationController
   def index
     @q = '上海'
+    @material_types = Material.where(level: 1, display: 1, deleted_at: nil).order(id: :asc)
     @cases = if @q.present?
       Cases.where('project_name LIKE ? OR business_type LIKE ? OR project_type LIKE ? OR project_location LIKE ? OR design_unit LIKE ?',
         "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%")
