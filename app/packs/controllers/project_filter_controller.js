770a3813de4d8eb980a778045d0bf8ee45528e4d
Eric-Guo <eric.guocz@gmail.com>
2021-11-24 16:37:23 +0800

    Control the div show or hide.

--- app/packs/controllers/project_filter_controller.js
+++ app/packs/controllers/project_filter_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['selectAll', 'selectItem', 'materialSpan', 'areaSpan', 'materialIconSvg', 'areaIconSvg']
+  static targets = [
+    'selectAll', 'selectItem', 'materialSpan', 'areaSpan', 'materialIconSvg', 'areaIconSvg',
+    'materialTypeDiv', 'materialDetailDiv', 'additionalDiv'
+  ]
   static values = {
     materialBarOpen: Boolean,
     areaBarOpen: Boolean,
@@ -32,10 +35,14 @@ export default class extends Controller {
     if (this.materialBarOpenValue) {
       this.materialSpanTarget.classList.remove('under-text-border');
       this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
+      this.materialTypeDivTarget.classList.add('hidden');
+      this.materialDetailDivTarget.classList.add('hidden');
       this.materialBarOpenValue = false;
     } else {
       this.materialSpanTarget.classList.add('under-text-border');
       this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 16H4L12 8L20 16Z"></path>`;
+      this.materialTypeDivTarget.classList.remove('hidden');
+      this.materialDetailDivTarget.classList.remove('hidden');
       this.materialBarOpenValue = true;
     }
     console.log(this.materialBarOpenValue);
@@ -46,10 +53,12 @@ export default class extends Controller {
     if (this.areaBarOpenValue) {
       this.areaSpanTarget.classList.remove('under-text-border');
       this.areaIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
+      this.additionalDivTarget.classList.add('hidden');
       this.areaBarOpenValue = false;
     } else {
       this.areaSpanTarget.classList.add('under-text-border');
       this.areaIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 16H4L12 8L20 16Z"></path>`;
+      this.additionalDivTarget.classList.remove('hidden');
       this.areaBarOpenValue = true;
     }
     console.log(this.areaBarOpenValue);
