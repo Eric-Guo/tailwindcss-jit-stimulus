153739e3c5fb02684d39c63a9dc0dbf9a42b05f8
秦嘉鹏 <759848795@qq.com>
2021-11-30 19:05:33 +0800

    修复手机端材料页面数据
    (cherry picked from commit 360935932343960840f416f515e7e9ed47cd2156)

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -4,7 +4,7 @@ class SamplesController < ApplicationController
   def show
     @sample = Sample.find_by(id: params[:id]) || Sample.find_by!(no: params[:id])
 
-    case_ids = CasesMaterial.select(:case_id).where(type: 1, sample_id: @sample.id).pluck(:case_id)
+    case_ids = CasesMaterial.select(:case_id).where(type_id: 1, sample_id: @sample.id).pluck(:case_id)
     @cases = Cases.where(id: case_ids).page(1).per(3)
     @other_samples = Sample.where.not(id: @sample.id).page(1).per(2)
 
@@ -16,7 +16,7 @@ class SamplesController < ApplicationController
   end
 
   def projects
-    case_ids = CasesMaterial.select(:case_id).where(type: 1, sample_id: params[:id]).pluck(:case_id)
+    case_ids = CasesMaterial.select(:case_id).where(type_id: 1, sample_id: params[:id]).pluck(:case_id)
     @cases = Cases.where(id: case_ids).page(params[:page]).per(params[:page_size])
   end
 end
