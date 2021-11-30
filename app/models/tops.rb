e1620ff960f7eac47541652338751dd896817f41
秦嘉鹏 <759848795@qq.com>
2021-11-30 18:50:28 +0800

    修改首页案例显示数据取值

--- app/models/tops.rb
+++ app/models/tops.rb
@@ -2,5 +2,6 @@
 
 class Tops < ApplicationRecord
   self.table_name = 'tops'
+  belongs_to :home_top_case, class_name: 'Cases', foreign_key: :case_id
   default_scope { where(deleted_at: nil) }
 end
