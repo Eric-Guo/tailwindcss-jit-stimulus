03c201fc5800c457da78549e03aa72a3d49d1343
Eric Guo <eric@cloud-mes.com>
2021-12-09 19:40:03 +0800

    extra lines.

--- app/models/color_system.rb
+++ app/models/color_system.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true SampleEnclosure
-
 class ColorSystem < ApplicationRecord
   has_many :material_product_color_systems, primary_key: :color_systems_id
   has_many :material_products, through: :material_product_color_system
--- app/models/sample_position_picture.rb
+++ app/models/sample_position_picture.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true SamplePositionPicture
-
 class SamplePositionPicture < ApplicationRecord
   default_scope { where(deleted_at: nil) }
 end
