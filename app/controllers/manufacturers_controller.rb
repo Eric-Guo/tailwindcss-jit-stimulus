4d0e9af094e831710ce54c588cf730aaa9da0c02
秦嘉鹏 <759848795@qq.com>
2021-12-13 19:14:27 +0800

    修改供应商数据显示, ui调整

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -6,9 +6,30 @@ class ManufacturersController < ApplicationController
 
   def show
     @manufacturer = Manufacturer.find(params[:id])
-    @materials = @manufacturer.materials.where(level: 3)
+    @materials = @manufacturer.materials
     @samples = @manufacturer.samples
-    @cases = Cases.joins(:materials).where(materials: { id: @materials.pluck(:id) }).distinct
-    @other_manufacturers = Manufacturer.joins(:materials).where.not(id: @manufacturer.id).where(materials: { id: @materials.pluck(:id) }).limit(4).distinct
+    @cases = Cases.joins(:samples).where(samples: { id: @samples.pluck(:id) }).distinct
+    material3_ids = @manufacturer.materials.where(level: 3).pluck(:id)
+    material2_ids = @manufacturer.materials.where(level: 2).pluck(:id)
+    if material2_ids.present?
+      material_ids = Material.where(level: 3).where(parent_id: material2_ids).pluck(:id)
+      material2_ids.push(*material_ids) if material_ids.present?
+    end
+    material1_ids = @manufacturer.materials.where(level: 1).pluck(:id)
+    if material1_ids.present?
+      material_ids = Material.where(level: [2, 3]).where("parent_id IN (:ids) OR grandpa_id IN (:ids)", ids: material1_ids)
+      material1_ids.push(*material1_ids) if material_ids.present?
+    end
+    @other_manufacturers = Manufacturer.select('manufacturers.*, materials.id as material_id').joins(:materials)
+      .where.not(id: @manufacturer.id)
+      .order(Arel.sql("( \
+        CASE \
+        WHEN materials.id IN (#{material3_ids.join(',').presence || 'null'}) THEN 3 \
+        WHEN materials.id IN (#{material2_ids.join(',').presence || 'null'}) THEN 2 \
+        WHEN materials.id IN (#{material1_ids.join(',').presence || 'null'}) THEN 1 \
+        ELSE 0 \
+        END \
+      ) DESC, id ASC"))
+      .limit(4).distinct
   end
 end
