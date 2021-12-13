5385c9b6dcec527bdd59d19b7401f861b6a99faf
Eric-Guo <eric.guocz@gmail.com>
2021-12-13 13:55:45 +0800

    Display locations also.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
     @material_types = Material.where(level: 1, display: 1, deleted_at: nil).order(id: :asc)
     @mat_ids = params[:ms].presence || []
     @selected_mats = Material.where(id: @mat_ids)
+    @locations = params[:l].presence || []
     @materials = Material.where(parent_id: Material.find_by(name: '石材').id, display: 1, deleted_at: nil).order(id: :asc)
     @cases = if @q.present?
       Cases.where('project_name LIKE ? OR business_type LIKE ? OR project_type LIKE ? OR project_location LIKE ? OR design_unit LIKE ?',
