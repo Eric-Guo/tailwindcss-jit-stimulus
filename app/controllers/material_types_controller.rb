2db936c31b7c4ef1cdac4d193ced1ffa3ec0bf82
Eric-Guo <eric.guocz@gmail.com>
2021-12-15 15:23:48 +0800

    Using from as folder name.
    (cherry picked from commit f2648b0076c965856b4313aeca3d5828414f7b66)

--- app/controllers/material_types_controller.rb
+++ app/controllers/material_types_controller.rb
@@ -2,6 +2,7 @@
 
 class MaterialTypesController < ApplicationController
   def show
+    @from = params[:from]
     @material_type = Material.find(params[:id])
     @materials = Material.where(parent_id: @material_type.id, display: 1, deleted_at: nil).order(id: :asc)
     render content_type: 'text/vnd.turbo-stream.html'
