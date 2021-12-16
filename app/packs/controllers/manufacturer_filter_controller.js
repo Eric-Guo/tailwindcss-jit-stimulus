63ab302570eab7d502dce1c59aacbbcd7f0a3760
Eric-Guo <eric.guocz@gmail.com>
2021-12-16 15:48:53 +0800

    two panel is close each other in manufacturers
    (cherry picked from commit 874b034d4a20c5cdef8a5df450d196d40d3d1b45)

--- app/packs/controllers/manufacturer_filter_controller.js
+++ app/packs/controllers/manufacturer_filter_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [
+    'activePanel',
     'selectAllMaterials', 'selectMaterialItem',
     'selectAllLocations', 'selectLocationItem',
     'materialSpan', 'areaSpan', 'materialIconSvg', 'areaIconSvg',
@@ -59,35 +60,33 @@ export default class extends Controller {
 
   expend_material(e) {
     if (this.materialBarOpenValue) {
-      this.materialSpanTarget.classList.remove('under-text-border');
-      this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
-      this.materialTypeDivTarget.classList.add('hidden');
-      this.materialDetailDivTarget.classList.add('hidden');
-      this.materialBarOpenValue = false;
+      this.closeMaterialPanel();
+      this.activePanelTarget.value = '';
     } else {
       this.materialSpanTarget.classList.add('under-text-border');
       this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 16H4L12 8L20 16Z"></path>`;
       this.materialTypeDivTarget.classList.remove('hidden');
       this.materialDetailDivTarget.classList.remove('hidden');
       this.materialBarOpenValue = true;
+      this.activePanelTarget.value = 'material';
     }
+    this.closeAreaPanel();
     e.preventDefault();
   }
 
   expend_area(e) {
     if (this.areaBarOpenValue) {
-      this.areaSpanTarget.classList.remove('under-text-border');
-      this.areaIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
-      this.locationDetailDivTarget.classList.add('hidden');
-      this.additionalDivTarget.classList.add('hidden');
-      this.areaBarOpenValue = false;
+      this.closeAreaPanel();
+      this.activePanelTarget.value = '';
     } else {
       this.areaSpanTarget.classList.add('under-text-border');
       this.areaIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 16H4L12 8L20 16Z"></path>`;
       this.locationDetailDivTarget.classList.remove('hidden');
       this.additionalDivTarget.classList.remove('hidden');
       this.areaBarOpenValue = true;
+      this.activePanelTarget.value = 'area';
     }
+    this.closeMaterialPanel();
     e.preventDefault();
   }
 
@@ -98,5 +97,21 @@ export default class extends Controller {
 
   clean_active(e) {
     this.matTypeLinkTargets.map(function(a) { a.classList.remove('font-bold'); });
+  }
+
+  closeMaterialPanel() {
+    this.materialSpanTarget.classList.remove('under-text-border');
+    this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
+    this.materialTypeDivTarget.classList.add('hidden');
+    this.materialDetailDivTarget.classList.add('hidden');
+    this.materialBarOpenValue = false;
+  }
+
+  closeAreaPanel() {
+    this.areaSpanTarget.classList.remove('under-text-border');
+    this.areaIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
+    this.locationDetailDivTarget.classList.add('hidden');
+    this.additionalDivTarget.classList.add('hidden');
+    this.areaBarOpenValue = false;
   }
 }
