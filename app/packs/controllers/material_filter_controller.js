956e9c44db37870af7a4aa0d0f5249f853d6c186
Eric Guo <eric@cloud-mes.com>
2021-12-17 15:42:31 +0800

    Fix material advanced materials.

--- app/packs/controllers/material_filter_controller.js
+++ app/packs/controllers/material_filter_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = [
     'activePanel',
     'selectAllMaterials', 'selectMaterialItem',
-    'selectAllLocations', 'selectLocationItem',
     'materialSpan', 'materialIconSvg',
     'materialTypeDiv',
     'materialDetailDiv', 'additionalDiv',
@@ -13,15 +12,11 @@ export default class extends Controller {
   static values = {
     materialBarOpen: Boolean,
     indeterminateMaterial: Boolean,
-    indeterminateLocation: Boolean,
   }
 
   connect() {
     if (this.indeterminateMaterialValue) {
       this.selectAllMaterialsTarget.indeterminate  = true;
-    }
-    if (this.indeterminateLocationValue) {
-      this.selectAllLocationsTarget.indeterminate  = true;
     }
   }
 
@@ -46,20 +41,14 @@ export default class extends Controller {
     e.preventDefault();
   }
 
-  select_all_locations(e) {
-    if(this.selectAllLocationsTarget.checked) {
-      this.selectLocationItemTargets.map(function(t) { t.checked = true; });
-    } else {
-      this.selectLocationItemTargets.map(function(t) { t.checked = false; });
-    }
-    const form = document.getElementById('material-form');
-    form.submit();
-    e.preventDefault();
-  }
-
   expend_material(e) {
     if (this.materialBarOpenValue) {
-      this.closeMaterialPanel();
+      this.materialSpanTarget.classList.remove('under-text-border');
+      this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
+      this.materialTypeDivTarget.classList.add('hidden');
+      this.materialDetailDivTarget.classList.add('hidden');
+      this.additionalDivTarget.classList.add('hidden');
+      this.materialBarOpenValue = false;
       this.activePanelTarget.value = '';
     } else {
       this.materialSpanTarget.classList.add('under-text-border');
@@ -80,14 +69,5 @@ export default class extends Controller {
 
   clean_active(e) {
     this.matTypeLinkTargets.map(function(a) { a.classList.remove('font-bold'); });
-  }
-
-  closeMaterialPanel() {
-    this.materialSpanTarget.classList.remove('under-text-border');
-    this.materialIconSvgTarget.innerHTML = `<path fill="currentColor" d="M20 8H4L12 16L20 8Z"></path>`;
-    this.materialTypeDivTarget.classList.add('hidden');
-    this.materialDetailDivTarget.classList.add('hidden');
-    this.additionalDivTarget.classList.add('hidden');
-    this.materialBarOpenValue = false;
   }
 }
