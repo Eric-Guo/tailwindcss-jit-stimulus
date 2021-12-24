bc363b5a6afa6c289c70df296986819a6c76f5e8
秦嘉鹏 <759848795@qq.com>
2021-12-24 11:07:29 +0800

    添加过渡效果

--- app/packs/controllers/need_register_controller.js
+++ app/packs/controllers/need_register_controller.js
@@ -55,7 +55,7 @@ export default class extends Controller {
     buttonGroupElem.appendChild(statusButton);
 
     const removeButton = document.createElement('button');
-    removeButton.className = 'text-neutral-300 hover:text-black hidden';
+    removeButton.className = 'text-neutral-300 hover:text-black transition-all duration-300 hidden';
     removeButton.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
