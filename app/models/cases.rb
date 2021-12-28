3a0f372e3fb7a0729df3b6ac23d9f9ff7e5c427f
Eric Guo <eric@cloud-mes.com>
2021-12-28 13:11:23 +0800

    Link area with cases.

--- app/models/cases.rb
+++ app/models/cases.rb
@@ -9,6 +9,7 @@ class Cases < ApplicationRecord
   has_many :case_samples, -> { where(type_id: 1) }, class_name: 'CasesMaterial', foreign_key: :case_id
   has_many :materials, through: :case_materials
   has_many :samples, through: :case_samples
+  belongs_to :area, foreign_key: :area_id
 
   default_scope { where(deleted_at: nil).where(display: 1) }
 
