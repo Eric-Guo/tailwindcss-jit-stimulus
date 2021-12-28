3fa08da4d86678f6dfe8a4dc5f775e947c4483d9
秦嘉鹏 <759848795@qq.com>
2021-12-28 17:35:22 +0800

    修改案例详情材料使用中的材料属性

--- app/models/cases_material.rb
+++ app/models/cases_material.rb
@@ -5,6 +5,14 @@ class CasesMaterial < ApplicationRecord
   belongs_to :case, class_name: 'Cases'
   belongs_to :material
   belongs_to :sample
+  belongs_to :manufacturer
 
   default_scope { where(deleted_at: nil).where.not(case_id: nil) }
+
+  def material_color
+    arr = []
+    arr.push(material.color) if material.color.present?
+    arr.push(color_code) if color_code.present?
+    arr.join(';')
+  end
 end
