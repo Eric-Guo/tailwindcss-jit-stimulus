f60895e005fc999268f216b38c28d67fa39df5b3
秦嘉鹏 <759848795@qq.com>
2021-12-29 11:20:58 +0800

    修改供应商详情tab未选中文字颜色
    (cherry picked from commit c9acc0cab5832d9c922b0c35edc35100c544ce3b)

--- app/packs/controllers/tabs_controller.js
+++ app/packs/controllers/tabs_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
   static values = {
     defaultTabName: String,
     tabActiveClass: String,
+    tabNormalClass: String,
   };
 
   connect() {
@@ -19,11 +20,14 @@ export default class extends Controller {
   setTabActive(tabName) {
     if (tabName) {
       const tabActClass = this.tabActiveClassValue.split(' ');
+      const tabNorClass = this.tabNormalClassValue.split(' ');
       this.tabTargets.forEach(elem => {
         if (elem.dataset.tabName === tabName) {
+          tabNorClass.forEach(item => elem.classList.remove(item));
           tabActClass.forEach(item => elem.classList.add(item));
         } else {
           tabActClass.forEach(item => elem.classList.remove(item));
+          tabNorClass.forEach(item => elem.classList.add(item));
         }
       });
       this.tabContentTargets.forEach(elem => {
