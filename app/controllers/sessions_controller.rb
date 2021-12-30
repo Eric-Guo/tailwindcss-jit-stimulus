d1711d322353cc52428339fd5a49892b3e29c12c
Eric Guo <eric@cloud-mes.com>
2021-12-30 16:16:27 +0800

    Fix login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-class SessionsController < ActionController::Base
+class SessionsController < ApplicationController
+  def new
+  end
+
   def destroy
     sign_out(current_user) if current_user.present?
     redirect_to root_path, alert: '成功登出'
