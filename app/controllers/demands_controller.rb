d4418af06e72bbb7e761f2950dc29780686e9012
秦嘉鹏 <759848795@qq.com>
2021-12-31 12:10:36 +0800

    修改提交需求登记数据类型

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -8,8 +8,8 @@ class DemandsController < ApplicationController
     res = Demand.submit({
       UserName: current_user.chinese_name,
       ClerkCode: current_user.clerk_code,
-      demandType: data[:cate],
-      material_id: data[:material],
+      demandType: data[:cate].to_i,
+      material_id: data[:material].to_i,
       description: data[:description],
       references: data[:files]
     })
