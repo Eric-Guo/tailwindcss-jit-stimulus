7e357b3675763c4d190326809eec830d1517b840
Eric Guo <eric@cloud-mes.com>
2021-12-31 10:55:00 +0800

    Should handle SELECT in different way.
    (cherry picked from commit 2b6a7a3a103e2782e2b25d72bd1db4c7628c49ca)

--- app/packs/controllers/project_filter_controller.js
+++ app/packs/controllers/project_filter_controller.js
@@ -31,8 +31,13 @@ export default class extends Controller {
   }
 
   remove(e) {
-    const check_box = document.getElementById(e.target.dataset.checkId);
-    check_box.checked = false;
+    const check_id = e.target.dataset.checkId;
+    const check_box = document.getElementById(check_id);
+    if (check_box.tagName == "SELECT") {
+      check_box.selectedIndex = -1;
+    } else {
+      check_box.checked = false;
+    }
     const form = document.getElementById('project-form');
     form.submit();
   }
