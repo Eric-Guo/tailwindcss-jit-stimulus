021e678b391c5352046b53b1b2dfa964866e0bc1
Eric Guo <eric.guocz@gmail.com>
2022-01-04 11:43:45 +0800

    Fix enter from portal.

--- app/controllers/openid_connect_controller.rb
+++ app/controllers/openid_connect_controller.rb
@@ -7,7 +7,7 @@ class OpenidConnectController < ApplicationController
     user = User.find_by!(email: @omniaut_auth.dig(:info, :email))
     original_url = request.env['omniauth.origin']
     sign_in user
-    if !original_url.nil? && original_url != 'https://sso.thape.com.cn/'
+    if !original_url.nil? && ['https://sso.thape.com.cn/', 'https://portal.thape.com.cn/'].exclude?(original_url)
       redirect_to original_url
     else
       redirect_to root_path
