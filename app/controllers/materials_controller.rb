6accdcf3643462cc45604b8a5f9fc2e87fc5ea2e
Eric-Guo <eric.guocz@gmail.com>
2022-01-06 15:10:38 +0800

    Query material no also.
    (cherry picked from commit fcea1d61e0b9abb45ce5973237d437cfc0c4ca58)

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -33,7 +33,7 @@ class MaterialsController < ApplicationController
         Material.where(id: mat_q_ids)
       else
         Material.left_joins(:samples).includes(:samples).where('materials.level <> 1')
-          .where('materials.name LIKE ? OR sample.genus LIKE ? OR sample.species LIKE ?', "%#{@q}%", "%#{@q}%", "%#{@q}%").distinct
+          .where('materials.no = ? OR materials.name LIKE ? OR sample.genus LIKE ? OR sample.species LIKE ?', @q, "%#{@q}%", "%#{@q}%", "%#{@q}%").distinct
       end
     else
       Material.where('materials.level <> 1')
