b4cd6031a9431096985b16ee87e4b3fdef2610d0
秦嘉鹏 <759848795@qq.com>
2022-02-21 14:41:25 +0800

    修改新闻来源标签

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,8 +4,4 @@ class News < ApplicationRecord
   has_many :news_materials
   has_many :materials, through: :news_materials
   default_scope { where(deleted_at: nil).where(display: 1) }
-
-  def cl_online?
-    cl_online_id.present? && cl_online_id != 0 && cl_online_id != ''
-  end
 end
