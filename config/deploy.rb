86cb9e5e5a40bf68e32911bc7553ee8ef2732a9d
Eric-Guo <eric.guocz@gmail.com>
2022-03-13 10:03:13 +0800

    upgrade capistrano
    (cherry picked from commit 1b96df88e86cce888f632413d027e93f5fa01b10)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.16.0'
+lock '~> 3.17.0'
 
 set :application, 'matlib'
 set :repo_url, 'https://git.thape.com.cn/rails/matlib.git'
