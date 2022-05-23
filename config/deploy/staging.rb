ec8981a5ebfe96c35fef7567d0e14afa5a74b388
Eric Guo <eric.guocz@gmail.com>
2022-05-23 15:42:30 +0800

    staging folder.
    (cherry picked from commit c11ea5c62c94f38697618d7cf174bf5035de59db)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,5 +4,6 @@ set :nginx_use_ssl, true
 set :branch, :matlib
 set :puma_service_unit_name, :puma_matlib_staging
 set :puma_systemctl_user, :system
+set :deploy_to, '/var/www/matlib_staging'
 
 server 'thape_matlib', user: 'matlib_staging', roles: %w{app db web}
