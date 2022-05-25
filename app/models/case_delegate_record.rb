e2e5ffcfe5f8c10dd352aa44292882b7252e32d2
ringo <ringobool@outlook.com>
2022-05-25 17:48:10 +0800

    修改图片bug及默认图片
    (cherry picked from commit 444ab76eb895f440ca2b389bde669e71d48b1c3a)

--- app/models/case_delegate_record.rb
+++ app/models/case_delegate_record.rb
@@ -9,11 +9,12 @@ class CaseDelegateRecord < ApplicationRecord
 
   def show_cover
     if web_cover != "" 
-      File.join('images', web_coverc)
+      File.join('images', web_cover)
     end
     if source_web_cover != "" 
       source_web_cover.sub( "http://jzw.thape.com.cn/upload/", "https://matlib.thape.com.cn/jzw_image/" )
     end
+    "https://matlib.thape.com.cn/test/uploads/project_bg.jpeg"
   end
 
 end
