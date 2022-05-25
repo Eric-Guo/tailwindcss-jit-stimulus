1818182e691090dc99d6435651976f1c49421592
秦嘉鹏 <759848795@qq.com>
2022-05-25 10:04:48 +0800

    修改个人中心案例列表读取条件

--- app/controllers/personal_centers_controller.rb
+++ app/controllers/personal_centers_controller.rb
@@ -2,27 +2,38 @@
 
 class PersonalCentersController < ApplicationController
   before_action :authenticate_user!
-  before_action :get_a_user
   before_action :get_no_read_message_count
   
   def projects
     title = params[:title].presence&.strip
     status = params[:status].presence&.strip
-    @list = CaseDelegate.joins(:record).where(user_id: @a_user.id)
+    lv10_positions = current_user.positions.where('post_level >= 10')
+    @list = CaseDelegate.joins(:record)
+    # 如果存在部门10级及以上的职位，则获取该部门下所有的案例信息，否则只获取被委派的案例
+    if lv10_positions.present?
+      @list = @list.where('delegate_user_id = ? OR department_id IN (?)', current_user.id, lv10_positions.pluck(:department_id))
+    else
+      @list = @list.where(delegate_user_id: current_user.id)
+    end
     @list = @list.where("`case_delegate_records`.project_name LIKE ?", "%#{title}%") if title.present?
-    @list = @list.where(status: params[:status]) if status.present?
+    if status.present?
+      s = CaseDelegate.web_status_hash[status.to_sym]
+      @list = @list.where(status: s[:names]) if status.present? if s.present?
+    end
   end
 
   def messages
     @list = Notification
-      # .where(notifiable_id: current_user.id)
+      .where(notifiable_type: 'cybros.user')
+      .where(notifiable_id: current_user.id)
       .order(created_at: :desc)
       .all
   end
 
   def set_message_read
     message = Notification
-      # .where(notifiable_id: current_user.id)
+      .where(notifiable_type: 'cybros.user')
+      .where(notifiable_id: current_user.id)
       .find(params[:id])
 
     if message.read_at.blank?
@@ -35,7 +46,8 @@ class PersonalCentersController < ApplicationController
 
   def rm_message
     message = Notification
-      # .where(notifiable_id: current_user.id)
+      .where(notifiable_type: 'cybros.user')
+      .where(notifiable_id: current_user.id)
       .find(params[:id])
     message.deleted_at = Time.now
     message.save
@@ -47,7 +59,7 @@ class PersonalCentersController < ApplicationController
   def demands
     @list = Demand
       .includes(:replies, :material)
-      # .where(clerk_code: current_user.clerk_code)
+      .where(clerk_code: current_user.clerk_code)
       .all
     puts @list
   end
@@ -56,13 +68,9 @@ class PersonalCentersController < ApplicationController
   end
 
   private
-    def get_a_user
-      @a_user = AUser.find_by(clerk_code: current_user.clerk_code)
-    end
-
     def get_no_read_message_count
       @no_read_message_count = Notification
-        # .where(notifiable_id: current_user.id)
+        .where(notifiable_id: current_user.id)
         .where('read_at IS NULL')
         .count
     end
