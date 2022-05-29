c38450ce8db05fdd2159854011022bc8f80b30fe
秦嘉鹏 <759848795@qq.com>
2022-05-29 23:39:58 +0800

    增加删除消息弹窗
    (cherry picked from commit 119e98a16f19ab79356a93294d8d011a85d17303)

--- app/packs/controllers/personal_center_messages_controller.js
+++ app/packs/controllers/personal_center_messages_controller.js
@@ -1,4 +1,21 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['modal', 'modalRmForm']
+  
+  setRmId(e) {
+    const formPath = e.target.dataset.formPath;
+    let modal = null;
+    if (this.hasModalTarget) {
+      modal = this.application.getControllerForElementAndIdentifier(this.modalTarget, 'modal');
+    }
+    if (modal) {
+      modal.open(e);
+    }
+    if (formPath && this.hasModalRmFormTarget) {
+      console.log(formPath);
+
+      this.modalRmFormTarget.action = formPath;
+    }
+  }
 }
