56bb6376bdd69dc73a7c47ec904d7c4de0ff80a3
秦嘉鹏 <759848795@qq.com>
2022-07-08 17:11:04 +0800

    优化案例列表

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,92 +7,68 @@ class ProjectsController < ApplicationController
   end
 
   def index
-    @page_size_options = [10, 15, 25, 40, 60, 85]
-    @page_size = params[:page_size].to_i > 0 ? params[:page_size].to_i : @page_size_options[0]
+    if turbo_frame_request?
+      @page_size_options = [10, 15, 25, 40, 60, 85]
+      @page_size = params[:page_size].to_i > 0 ? params[:page_size].to_i : @page_size_options[0]
 
-    @panel_name = params[:pn].presence
-    @q = ActiveRecord::Base::sanitize_sql(params[:q]&.strip)
+      @q = ActiveRecord::Base::sanitize_sql(params[:q]&.strip)
 
-    @material_types = Material.where(level: 1).order(no: :asc)
-    mat_ids = (params[:ms].presence || []).reject(&:blank?)
-    @selected_mats = if mat_ids.present?
-      Material.where(id: mat_ids)
-    else
-      Material.none
-    end
-    @selected_mat_parent_id = @selected_mats.collect(&:parent_id).first || 1
-    @area_ids = (params[:l].presence || []).reject(&:blank?).map(&:to_i)
+      mat_ids = (params[:ms].presence || []).reject(&:blank?)
 
-    @project_type = params[:project_type].presence
-    @need_ecm_files = params[:ecm_files] == 'on'
-    @has_sample = params[:has_sample] == 'on'
-    @has_demonstration = params[:has_demonstration] == 'on'
-    @is_th_internal = params[:is_th_internal] == 'on'
+      @area_ids = (params[:l].presence || []).reject(&:blank?).map(&:to_i)
 
-    @all_materials = Material.where(parent_id: @selected_mat_parent_id).order(no: :asc)
-    @selected_all_materials = @all_materials.pluck(:id) == mat_ids.collect(&:to_i)
-    @selected_none_materials = (@all_materials.pluck(:id) & mat_ids.collect(&:to_i)).blank?
+      @project_type = params[:project_type].presence
+      @need_ecm_files = params[:ecm_files] == 'on'
+      @has_sample = params[:has_sample] == 'on'
+      @has_demonstration = params[:has_demonstration] == 'on'
+      @is_th_internal = params[:is_th_internal] == 'on'
 
-    @selected_all_locations = Cases.project_locations.collect(&:area_id) == @area_ids
-    @selected_none_locations = (Cases.project_locations.collect(&:area_id) & @area_ids).blank?
-
-    cases_with_query = if @q.present?
-      mat_ids = q_return_mat_ids(@q)
+      @cases = Cases.order(updated_at: :desc)
+      
+      if @q.present?
+        q_mat_ids = MaterialAndSample.where(sample_id: nil).where('material_name LIKE :q_like OR parent_material_name LIKE :q_like OR grandpa_material_name LIKE :q_like', q_like: "%#{@q}%").pluck(:material_id)
+        case_ids = CasesMaterial.where(type_id: 2).where(material_id: q_mat_ids).pluck(:case_id)
+        @cases = @cases.where('project_name LIKE :q_like OR business_type LIKE :q_like OR project_type LIKE :q_like OR project_location LIKE :q_like OR design_unit LIKE :q_like OR id IN (:case_ids)', q_like: "%#{@q}%", case_ids: case_ids)
+      end
 
       if mat_ids.present?
-        Cases.left_joins(:case_materials).where(case_materials: { material_id:  mat_ids})
-      else
-        Cases.where('project_name LIKE ? OR business_type LIKE ? OR project_type LIKE ? OR project_location LIKE ? OR design_unit LIKE ?', "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%")
+        case_ids = CasesMaterial.where(type_id: 2).where(material_id: mat_ids).pluck(:case_id)
+        @cases = @cases.where('id IN (?)', case_ids)
       end
-    else
-      Cases.all
+
+      if @area_ids.present?
+        @cases = @cases.where('area_id IN (?)', @area_ids)
+      end
+
+      if @project_type.present?
+        @cases = @cases.where(project_type: @project_type)
+      end
+
+      if @need_ecm_files.present?
+        @cases = @cases.where(lmkzsc_id: Lmkzsc.pluck(:id))
+      end
+
+      if @has_sample.present?
+        case_ids = CasesMaterial.where(type_id: 1).pluck(:case_id)
+        @cases = @cases.where('id IN (?)', case_ids)
+      end
+
+      if @has_demonstration.present?
+        @cases = @cases.where(is_da: true)
+      end
+
+      if @is_th_internal.present?
+        @cases = @cases.where(is_th: true)
+      end
+
+      @total = @cases.count
+      @cases = @cases.page(@page).per(@page_size)
+
+      @material_in_cases = CasesMaterial.joins(:material).where(type_id: 2).where(case_id: @cases.collect(&:id)).pluck('case_id, materials.name')
+
+      render 'list'
     end
 
-    cases_with_materials = if mat_ids.present?
-      cases_with_query.includes(:case_materials).where(case_materials: { material_id: mat_ids.append(@selected_mat_parent_id) })
-    else
-      cases_with_query
-    end
-
-    cases_with_location = if @area_ids.present?
-      cases_with_materials.where(area_id: @area_ids)
-    else
-      cases_with_materials
-    end
-
-    cases_with_project_type = if @project_type.present?
-      cases_with_location.where('FIND_IN_SET(?,project_type)', @project_type)
-    else
-      cases_with_location
-    end
-
-    cases_ecm = if @need_ecm_files
-      cases_with_project_type.where("JSON_EXTRACT(`ecm_files`, '$[0]') is not null")
-    else
-      cases_with_project_type
-    end
-
-    cases_has_sample = if @has_sample
-      cases_ecm.joins(:case_samples)
-    else
-      cases_ecm
-    end
-
-    cases_has_demonstration = if @has_demonstration
-      cases_has_sample.where(is_da: true)
-    else
-      cases_has_sample
-    end
-
-    cases_is_th_internal = if @is_th_internal
-      cases_has_demonstration.where(is_th: true)
-    else
-      cases_has_demonstration
-    end
-
-    @total = cases_is_th_internal.count
-    @cases = cases_is_th_internal.page(@page).per(@page_size)
-    @material_in_cases = CasesMaterial.joins(:material).where(case_id: @cases.collect(&:id)).pluck('case_id, materials.name')
   end
 
   def show
