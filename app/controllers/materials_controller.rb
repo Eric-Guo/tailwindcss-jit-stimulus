af650bb09c8590229303d2bcb93aa4c9dee78375
秦嘉鹏 <759848795@qq.com>
2022-07-08 13:03:47 +0800

    优化材料列表表单

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -8,74 +8,68 @@ class MaterialsController < ApplicationController
   end
 
   def index
-    @page_size_options = [24, 32, 48, 72, 104]
-    @page_size = params[:page_size].to_i > 0 ? params[:page_size].to_i : @page_size_options[0]
+    if turbo_frame_request?
+      @page_size_options = [24, 32, 48, 72, 104]
+      @page_size = params[:page_size].to_i > 0 ? params[:page_size].to_i : @page_size_options[0]
 
-    @panel_name = params[:pn].presence
-    @q = ActiveRecord::Base::sanitize_sql(params[:q]&.strip)
+      @q = ActiveRecord::Base::sanitize_sql(params[:q]&.strip)
 
-    @material_types = Material.where(level: 1).order(no: :asc)
-    mat_ids = (params[:ms].presence || []).reject(&:blank?).collect(&:to_i)
-    @selected_mats = if mat_ids.present?
-      Material.where(id: mat_ids).order(no: :asc)
-    else
-      Material.none
+      mat_ids = (params[:ms].presence || []).reject(&:blank?).collect(&:to_i)
+
+      @color_system = ColorSystem.find_by id: params[:color_system].presence
+
+      @price_start = params[:price_start].presence
+      @price_end = params[:price_end].presence
+
+      @area_id = params[:area_id].presence
+
+      @list = MaterialAndSample.where.not(material_level: 1).order(material_no: :asc, sample_id: :asc)
+
+      # 关键词
+      if @q.present?
+        @list = @list.where('material_name LIKE :q_like OR parent_material_name LIKE :q_like OR grandpa_material_name LIKE :q_like', q_like: "%#{@q}%")
+      end
+
+      # 材料筛选
+      if mat_ids.present?
+        @list = @list.where('material_id IN (:ids) OR parent_material_id IN (:ids)', ids: mat_ids)
+      end
+
+      # 色系
+      if @color_system.present?
+        color_mat_ids = MaterialProduct.joins(:material_product_color_systems).where(material_product_color_systems: { color_systems_id: @color_system.id }).pluck(:material_id)
+        color_sam_ids = SampleColorSystem.where(color_systems_id: @color_system.id).pluck(:sample_id)
+        @list = @list.where('(sample_id IS NULL AND material_id IN (?)) OR (sample_id IN (?))', color_mat_ids, color_sam_ids)
+      end
+
+      # 价格
+      if @price_start.present? && @price_end.present?
+        price_mat2_ids = MaterialInfo.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:material_id)
+        price_mat3_ids = MaterialProduct.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:material_id)
+        price_sam_ids = Sample.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:id)
+        @list = @list.where('(sample_id IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
+      elsif @price_start.present?
+        price_mat2_ids = MaterialInfo.where('low_price <= ?', @price_start).pluck(:material_id)
+        price_mat3_ids = MaterialProduct.where('low_price <= ?', @price_start).pluck(:material_id)
+        price_sam_ids = Sample.where('low_price <= ?', @price_start).pluck(:id)
+        @list = @list.where('(sample_id IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
+      elsif @price_end.present?
+        price_mat2_ids = MaterialInfo.where('high_price >= ?', @price_end).pluck(:material_id)
+        price_mat3_ids = MaterialProduct.where('high_price >= ?', @price_end).pluck(:material_id)
+        price_sam_ids = Sample.where('high_price >= ?', @price_end).pluck(:id)
+        @list = @list.where('(sample_id IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
+      end
+
+      if @area_id.present?
+        area_mat_ids = MaterialProduct.joins(:material_product_areas).where(material_product_areas: { area_id: @area_id }).pluck(:material_id)
+        @list = @list.where('material_id IN (?)', area_mat_ids)
+      end
+
+      @total = @list.count
+      @list = @list.page(@page).per(@page_size)
+
+      render 'list'
     end
-    @selected_mat_parent_id = @selected_mats.collect(&:parent_id).first || 1
-
-    @color_system = ColorSystem.find_by id: params[:color_system].presence
-    @price_start = params[:price_start].presence
-    @price_end = params[:price_end].presence
-    @area_id = params[:area_id].presence
-
-    @all_materials = Material.where(parent_id: @selected_mat_parent_id).order(no: :asc)
-    @selected_all_materials = @all_materials.pluck(:id) == mat_ids.collect(&:to_i)
-    @selected_none_materials = (@all_materials.pluck(:id) & mat_ids.collect(&:to_i)).blank?
-
-    @list = MaterialAndSample.where.not(material_level: 1).order(material_no: :asc, sample_id: :asc)
-
-    # 关键词
-    if @q.present?
-      @list = @list.where('material_name LIKE :q_like OR parent_material_name LIKE :q_like OR grandpa_material_name LIKE :q_like', q_like: "%#{@q}%")
-    end
-
-    # 材料筛选
-    if mat_ids.present?
-      @list = @list.where('material_id IN (:ids) OR parent_material_id IN (:ids)', ids: mat_ids)
-    end
-
-    # 色系
-    if @color_system.present?
-      color_mat_ids = MaterialProduct.joins(:material_product_color_systems).where(material_product_color_systems: { color_systems_id: @color_system.id }).pluck(:material_id)
-      color_sam_ids = SampleColorSystem.where(color_systems_id: @color_system.id).pluck(:sample_id)
-      @list = @list.where('(sample IS NULL AND material_id IN (?)) OR (sample_id IN (?))', color_mat_ids, color_sam_ids)
-    end
-
-    # 价格
-    if @price_start.present? && @price_end.present?
-      price_mat2_ids = MaterialInfo.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:material_id)
-      price_mat3_ids = MaterialProduct.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:material_id)
-      price_sam_ids = Sample.where('low_price <= ? AND high_price >= ?', @price_start, @price_end).pluck(:id)
-      @list = @list.where('(sample IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
-    elsif @price_start.present?
-      price_mat2_ids = MaterialInfo.where('low_price <= ?', @price_start, @price_end).pluck(:material_id)
-      price_mat3_ids = MaterialProduct.where('low_price <= ?', @price_start, @price_end).pluck(:material_id)
-      price_sam_ids = Sample.where('low_price <= ?', @price_start, @price_end).pluck(:id)
-      @list = @list.where('(sample IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
-    elsif @price_end.present?
-      price_mat2_ids = MaterialInfo.where('high_price >= ?', @price_start, @price_end).pluck(:material_id)
-      price_mat3_ids = MaterialProduct.where('high_price >= ?', @price_start, @price_end).pluck(:material_id)
-      price_sam_ids = Sample.where('high_price >= ?', @price_start, @price_end).pluck(:id)
-      @list = @list.where('(sample IS NULL AND material_id IN (?)) OR (sample_id IN (?))', [*price_mat2_ids, *price_mat3_ids], price_sam_ids)
-    end
-
-    if @area_id.present?
-      area_mat_ids = MaterialProduct.join(:material_product_areas).where(material_product_areas: { area_id: @area_id }).pluck(:material_id)
-      @list = @list.where('material_id IN (?)', area_mat_ids)
-    end
-
-    @total = @list.count
-    @list = @list.page(@page).per(@page_size)
   end
 
   def show
---
+++ app/packs/controllers/material_form_list_controller.js
@@ -0,0 +1,109 @@
+import { Controller } from '@hotwired/stimulus';
+import { debounce } from 'lodash-es';
+
+export default class extends Controller {
+  static targets = ['form', 'list', 'submitBtn', 'panel', 'material1', 'material2List', 'materialAllCheckbox', 'materialItemCheckbox']
+
+  mat_ids = []
+  material1Id = null
+  material2s = []
+
+  connect() {
+    const activeMaterial1Target = this.material1Targets.find(item => item.classList.contains('font-bold'));
+    if (activeMaterial1Target) {
+      this.material1Id = activeMaterial1Target.dataset.id;
+      this.setMaterial2s(JSON.parse(activeMaterial1Target.dataset.children));
+    }
+  }
+
+  togglePanel(e) {
+    const span = e.currentTarget.querySelector('span');
+    const svg = e.currentTarget.querySelector('svg');
+    if (this.panelTarget.classList.contains('hidden')) {
+      this.panelTarget.classList.remove('hidden');
+      span.classList.add('under-text-border');
+      svg.classList.add('rotate-180');
+    } else {
+      this.panelTarget.classList.add('hidden');
+      span.classList.remove('under-text-border');
+      svg.classList.remove('rotate-180');
+    }
+  }
+
+  activeMaterial1(e) {
+    const isActive = e.currentTarget.classList.contains('font-bold');
+    if (!isActive) {
+      this.material1Targets.forEach(item => item.classList.remove('font-bold'));
+      e.currentTarget.classList.add('font-bold');
+      this.material1Id = e.currentTarget.dataset.id;
+      this.setMaterial2s(JSON.parse(e.currentTarget.dataset.children));
+    }
+  }
+
+  setMaterial2s(data) {
+    this.material2s = data;
+    this.mat_ids = [];
+    this.renderMaterial2s();
+  }
+
+  renderMaterial2s() {
+    const html = this.material2s.map(item => `
+      <div class="my-2.5 flex items-start">
+        <input
+          id="material_${item.id}"
+          name="ms[]"
+          value="${item.id}"
+          type="checkbox"
+          ${this.mat_ids.includes(item.id) ? 'checked' : ''}
+          class="mt-1 focus:ring-gray-50 text-gray-600 border-gray-300"
+          data-material-form-list-target="materialItemCheckbox"
+          data-action="change->material-form-list#changeMaterialItemCheckbox"
+        >
+        <label for="material_${item.id}" class="text-gray-700 ml-2 mt-px">${item.title}</label>
+      </div>
+    `).join('');
+    this.material2ListTarget.innerHTML = html;
+    this.updateMaterialCheckboxStatus();
+  }
+  
+  updateMaterialCheckboxStatus() {
+    this.materialAllCheckboxTarget.disabled = this.materialItemCheckboxTargets.length === 0;
+    if (this.mat_ids.length > 0) {
+      if (this.mat_ids.length === this.materialItemCheckboxTargets.length) {
+        this.materialAllCheckboxTarget.checked = true;
+        this.materialAllCheckboxTarget.indeterminate = false;
+      } else {
+        this.materialAllCheckboxTarget.checked = false;
+        this.materialAllCheckboxTarget.indeterminate = true;
+      }
+    } else {
+      this.materialAllCheckboxTarget.checked = false;
+      this.materialAllCheckboxTarget.indeterminate = false;
+    }
+  }
+
+  changeMaterialItemCheckbox(e) {
+    const id = Number(e.target.value);
+    const checked = e.target.checked;
+    if (checked) {
+      if (!this.mat_ids.includes(id)) this.mat_ids.push(id);
+    } else {
+      this.mat_ids = this.mat_ids.filter(item => item != id);
+    }
+    this.updateMaterialCheckboxStatus();
+    this.debounceSubmitForm();
+  }
+
+  changeMaterialAllCheckbox(e) {
+    const checked = e.target.checked;
+    this.mat_ids = checked ? this.material2s.map(item => item.id) : [];
+    this.renderMaterial2s();
+    this.debounceSubmitForm();
+  }
+
+  debounceSubmitForm = debounce(this.submitForm, 300)
+
+  submitForm() {
+    this.formTarget.requestSubmit();
+  }
+}
