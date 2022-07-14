33dfa171ebf634b96da9fac4a9cdaa3c969949ae
秦嘉鹏 <759848795@qq.com>
2022-07-14 16:16:24 +0800

    修改供应商推荐跳转详情规则
    (cherry picked from commit ff3f18f552db9816c931e8c050a0fac65f02b6af)

--- app/models/manufacturer_recommend.rb
+++ app/models/manufacturer_recommend.rb
@@ -3,10 +3,15 @@
 class ManufacturerRecommend < ApplicationRecord
   belongs_to :material
   has_many :cases, class_name: 'ManufacturerRecommendCase'
+  belongs_to :manufacturer
 
   default_scope { where(deleted_at: nil) }
 
   def is_th_co_text
     is_th_co ? '是' : '否'
+  end
+
+  def to_manufacturer?
+    status == 'manufacturer_recommend_approved' && manufacturer.present?
   end
 end
