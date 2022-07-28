43f601a63d7187626787968c0bc8a482861a0b88
秦嘉鹏 <759848795@qq.com>
2022-07-28 18:21:26 +0800

    帮助中心新增下载按钮
    (cherry picked from commit af12786e85ecacd595b1cad177a9ef8e64cdea19)

--- app/controllers/personal_centers_controller.rb
+++ app/controllers/personal_centers_controller.rb
@@ -146,7 +146,8 @@ class PersonalCentersController < ApplicationController
   end
 
   def helper_center
-    @file_path = 'uploads/file/00e4d0a485f45450ab0e217145a1b63a_20220728180707.jpg';
+    @pdf_path = 'uploads/file/f08947cbf4f7e2a688ca1ca7522b5619_20220728182007.pdf';
+    @img_path = 'uploads/file/00e4d0a485f45450ab0e217145a1b63a_20220728180707.jpg';
   end
 
   private
