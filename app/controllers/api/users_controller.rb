7885b253505e1fbeb5799a169b4ef3c75ed8b394
秦嘉鹏 <759848795@qq.com>
2022-11-22 15:51:18 +0800

    新增API - 个人中心帮助中心

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,5 +9,10 @@ module Api
         render json: nil
       end
     end
+
+    def helpers
+      @pdf_path = 'uploads/file/f08947cbf4f7e2a688ca1ca7522b5619_20220728182007.pdf';
+      @img_path = 'uploads/file/00e4d0a485f45450ab0e217145a1b63a_20220728180707.jpg';
+    end
   end
 end
