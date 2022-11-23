6ff1e651621bc1156c4586db478be88d08b017e5
秦嘉鹏 <759848795@qq.com>
2022-11-23 12:01:13 +0800

    修改企业wx登录接口
    (cherry picked from commit c2d0e3c4d4e97f87ac716b28bcee0afe0ceb24e9)

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -25,8 +25,7 @@ module Api
         raise Exception.new('获取用户信息失败') unless Current.user.present?
 
         sign_in Current.user
-
-        render json: { message: '登录成功' }
+        return render json: { message: '登录成功' }
       end
     end
   end
