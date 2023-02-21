fc6222eb8cdb333bd5a65b48547e1141e9e1d1a3
Eric Guo <eric.guocz@gmail.com>
2023-02-21 10:10:40 +0800

    gem and yarn upgrade
    (cherry picked from commit 1a722a22b465579213e8a8c582a662981b49fa6e)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock '~> 3.17.2'
 
 set :application, 'matlib'
 set :repo_url, 'git@git.thape.com.cn:rails/matlib.git'
