450af2aa273febc535260d0b5eb94f4d30f585dd
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-06-28 16:38:01 +0800

    优化访客登陆
    (cherry picked from commit bf21cba2674023007619337709f3699bd30981b9)

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -6,9 +6,13 @@ class VisitorController < ApplicationController
   skip_before_action :set_footer_info
 
   def login
-    visitor = Visitor.where(enabled: true).where('expired_at > ?', Time.now).find_by!(code: params[:code])
-    sign_in visitor
-    redirect_to root_path
+    visitor = Visitor.where(enabled: true).where('expired_at > ?', Time.now).find_by(code: params[:code])
+    if visitor
+      sign_in visitor
+      redirect_to root_path
+    else
+      render html: "<h1>邀请链接已失效，<a href=\"/\">返回首页</a></h1>".html_safe
+    end
   end
 
   def logout
