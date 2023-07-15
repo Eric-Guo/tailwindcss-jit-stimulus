a43ee78fa094054f5e979449e9ea65627a93bafb
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-07-15 09:49:48 +0800

    个人中心访客邀请添加职级限制
    (cherry picked from commit c0ec762b47912beeff1ad4f8287616fab55b66e4)

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -7,6 +7,10 @@ class Visitor < ActiveRecord::Base
 
   default_scope { where(deleted_at: nil) }
 
+  def self.user_auth?(current_user)
+    current_user.super_staff? || current_user.main_position_level >= 9
+  end
+
   def effective?
     (self.enabled && self.expired_at && self.expired_at > Time.now).present?
   end
