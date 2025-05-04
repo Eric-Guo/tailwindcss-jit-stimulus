7999ae4f32103e3115f5db55fc6904d101638c29
Eric-Guo <eric.guocz@gmail.com>
2025-05-04 13:50:03 +0800

    Bump ruby to 3.2.8
    (cherry picked from commit 2046fde444888486d2abdc8355f3831c533e4136)

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ append :linked_dirs, 'log', 'storage', 'tmp/pids', 'tmp/cache', 'tmp/sockets', '
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user
-set :rbenv_ruby, '3.2.3'
+set :rbenv_ruby, '3.2.8'
 
 set :puma_init_active_record, true
 set :puma_phased_restart, true
